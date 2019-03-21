bcf7af0492c4083e3fd76a89546813a948a261a9
sofia <sofiawanchod@gmail.com>
2019-03-20 17:14:13 -0700

    added the nav bar

--- src/index.php
+++ src/index.php
@@ -16,10 +16,46 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-   
-    <div class="container">
-        <h1>hello world</h1>
-    </div>
+
+    <!-- Nav bar -->
+
+    <nav class="navbar navbar-expand-md navbar-dark" id="nav">
+  <a class="navbar-brand" href="#">VINTACH</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active" id="active">
+        <a class="nav-link" href="#">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">MUSIC</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">FILM</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ELECTRONICS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">DECOR</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav" id="credentials">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">SIGN UP</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="#">LOGIN</a>
+        </li>
+    </ul>
+  </div>
+
+    <!-- Carrousel -->
+
+
+</nav>
 
 </body>
 </html>
