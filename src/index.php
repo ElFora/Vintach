6788b168093e7d0cde92165c184a1e8082e0d479
sofia <sofiawanchod@gmail.com>
2019-03-23 02:11:56 -0700

    asnd

--- src/index.php
+++ src/index.php
@@ -9,18 +9,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css">
-    <script src="./scripts/jquery.min.js"></script>
-    <script type="text/javascript" src="./scripts/jquery.breakpoints.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>    
+    <script src="js/script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!-- <link href="css/custom.scss" rel="stylesheet"> -->
     <link href="css/estilos.css" rel="stylesheet">
 
     <title>Vintage Store</title>
 </head>
 <body>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+    
     <!-- Nav bar -->
 
 <nav class="navbar navbar-expand-lg navbar-dark" id="nav">
@@ -102,5 +100,7 @@
         </div>
       </div>
     <!-- End Carousel -->
+
+
 </body>
 </html>
---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+// @media screen and (max-width: 1024px){
+//   body:after {
+//       content: "extra-large";
+//   }
+// }
+// @media screen and (max-width: 768px){
+//   body:after {
+//       content: "large";
+//   }
+// }
