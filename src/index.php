800fe72edd4e9a72b337be71d1ba3e21ac610b0b
sofia <sofiawanchod@gmail.com>
2019-03-24 03:26:36 -0700

    sign up page??

--- src/index.php
+++ src/index.php
@@ -16,48 +16,17 @@
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
     <!-- <link href="css/custom.scss" rel="stylesheet"> -->
     <link href="css/estilos.css" rel="stylesheet">
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/png" href="icons/favicon.png"/>
+    <link rel="shortcut icon" type="image/png" href="http://index.php/favicon.png"/>
 
-    <title>Vintage Store</title>
+
+    <title>Vintache</title>
 </head>
-<body>
+<body class="index">
     
     <!-- Nav bar -->
-
-<nav class="navbar navbar-expand-lg navbar-dark" id="nav">
-  <a class="navbar-brand" href="#">VINTACH</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarResponsive">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active" id="active">
-        <a class="nav-link" href="index.php">HOME</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="cassettes.php">CASSETTES</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="vhs.php">VHS</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="profile.php">PROFILE</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="checkout.php">CHECKOUT</a>
-      </li>
-    </ul>
-    <div class="yellow-font">
-    <ul class="navbar-nav" id="credentials">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">SIGN UP</a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="#">LOGIN</a>
-        </li>
-    </ul>
-    </div>
-  </div>
-  </nav>
+    <?php include("includes/nav.php");?>
     <!-- End Nav bar -->
 
     <!-- Carousel -->
@@ -107,3 +76,13 @@
 
 </body>
 </html>
+
+<script>
+$(document).ready(function(){
+    $("body").on({
+        mouseenter: function(){
+            $(this).css("background-color", "#5B4350");
+        },  
+    });    
+});
+</script>
---
+++ src/signup.php
@@ -0,0 +1,79 @@
+<?php 
+   
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js-bootstrap/bootstrap.js"></script>
+    <script src="js-bootstrap/bootstrap.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+    <!-- <link href="css/custom.scss" rel="stylesheet"> -->
+    <link href="css/estilos.css" rel="stylesheet">
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/png" href="icons/favicon.png"/>
+    <link rel="shortcut icon" type="image/png" href="http://index.php/favicon.png"/>
+
+    <title>Vintache</title>
+</head>
+<body background="images/fotos_signup.png">
+    
+    <!-- Nav bar -->
+    <?php include("includes/nav.php");?>
+    <!-- End Nav bar -->
+
+    <!-- Container -->
+<div class="container sign-up">
+  <div class="row">
+    <div class="col-lg-8">
+      <div class="sign-up">
+          <div class="photo-container">
+              <img src="images/girl.jpg">
+          </div> 
+        </div>  
+    </div>
+    <div class="col-lg-4">
+      <div class="form-thing">
+          <form class="sign-up-form">
+              <div class="form-group">
+                <label for="formGroupExampleInput">Name</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+              </div>
+              <div class="form-group">
+                <label for="formGroupExampleInput">Address</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+              </div>
+              <div class="form-group">
+                  <label for="exampleFormControlInput1">Email</label>
+                  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+          </form>
+      </div>
+    </div>  
+  </div>
+</div>
+    <!-- Container -->
+
+
+</body>
+</html>
+
+<!-- <script>
+$(document).ready(function(){
+    $("body").on({
+        mouseenter: function(){
+            $(".bg").css("background-image", "url('/images/fotos_signup.png')", "no-repeat", "width=100%");
+        },  
+    });    
+});
+</script> -->
